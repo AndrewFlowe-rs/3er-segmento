5fa1be599ef21e8472a36d0f6b170ca949c2d4f4
andrewflower-rs <arnaldomahone@hotmail.com>
2024-04-07 00:03:04 -0300

    second commit

--- src/db/models/movie.js
+++ src/db/models/movie.js
@@ -1,42 +1,35 @@
 module.exports = (Sequelize, DataTypes) => {
-    const movie = sequalize.define("movie", {
+    const movie = sequalize.define("movie" , {
         id: {
-            type: DataTypes.INTEGER,
-            primarikey: true
+            type : DataTypes.INTEGER,
+            primarikey:true
+        } ,
+        created_at : {
+            type : DataTypes.DATEONLY,
         },
-        created_at: {
-            type: DataTypes.DATEONLY,
+        updated_at :{
+            type : DataTypes.DATEONLY,
         },
-        updated_at: {
-            type: DataTypes.DATEONLY,
+        title:{
+            type : DataTypes.STRING(100),
         },
-        title: {
-            type: DataTypes.STRING(100),
+        rating : {
+            type : DataTypes.DECIMAL(1,1),
         },
-        rating: {
-            type: DataTypes.DECIMAL(1, 1),
+        awards : {
+            type : DataTypes.INTEGER,
         },
-        awards: {
-            type: DataTypes.INTEGER,
+        release_date : {
+            type : DataTypes.DATEONLY,
         },
-        release_date: {
-            type: DataTypes.DATEONLY,
+        length : {
+            type : DataTypes.INTEGER,
         },
-        length: {
-            type: DataTypes.INTEGER,
-        },
-        genre_id: {
-            type: DataTypes.INTEGER,
-            references: {
-                model: 'Genre', // Nombre del modelo de la tabla relacionada
-                key: 'id',
-            }
-        }
 
-    }, {
-        tableName: "movies",
+    },{
+        tableName:"movies",
         timestamps: false
 
     });
-    return movie;
+        return movie ;
 };
