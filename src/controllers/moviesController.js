edf3f803a5befbfc218dbd4b77e86a0aa3d703b2
AndrewFlowe-rs <arnaldomahone@gmail.com>
2024-05-14 23:18:59 -0300

    se agregan cambios

--- src/controllers/moviesController.js
+++ src/controllers/moviesController.js
@@ -1,5 +1,14 @@
 const db = require("../db/models")
 const { Op } = db.Sequelize
+const dateFormat = function (date) {
+  const year = date.getFullYear();
+  let month = date.getMonth() + 1;
+  month = month < 10 ? "0" + month : month;
+  let day = date.getDate() + 1;
+  day = day < 10 ? "0" + day : day;
+  const newFormat = `${year}-${month}-${day}`;
+  return newFormat;
+}
 
 module.exports = {
   list: (req, res) => {
@@ -74,5 +83,70 @@ module.exports = {
       .catch((err) => {
         res.send(err.message)
       })
-  }
+  },
+  add: function (req, res) {
+    db.Genre.findAll()
+        .then(genres => {
+            res.render('moviesAdd', {
+                genres
+            })
+        })
+},
+create: function (req, res) {
+    db.Movie.create({
+        title: req.body.title,
+        rating: req.body.rating,
+        awards: req.body.awards,
+        release_date: req.body.release_date,
+        length: req.body.length,
+        genre_id: req.body.genre_id
+    }),
+
+        res.redirect('/movies')
+},
+edit: function (req, res) {
+   db.Genre.findAll()
+        .then(genres => {
+            db.Movie.findByPk(req.params.id, {
+                include: [{
+                    association: 'genre'
+                }]
+            })
+                .then(Movie => {
+                    const formatDate = dateFormat(Movie.release_date)
+                    res.render('moviesEdit', { Movie, genres, formatDate });
+                })
+        })
+},
+update: function (req, res) {
+    db.Movie.update({
+        title: req.body.title,
+        rating: req.body.rating,
+        awards: req.body.awards,
+        release_date: req.body.release_date,
+        length: req.body.length,
+        genre_id: req.body.genre_id
+    }, {
+        where: {
+            id: req.params.id
+        }
+    })
+
+    res.redirect('/movies');
+
+},
+borrar: function (req, res) {
+    db.Movie.findByPk(req.params.id)
+        .then(Movie => {
+            res.render('moviesDelete', { Movie });
+        })
+},
+destroy: function (req, res) {
+    db.Movie.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    res.redirect('/movies');
+}
 }
--- src/routes/moviesRoutes.js
+++ src/routes/moviesRoutes.js
@@ -6,6 +6,12 @@ router.get('/movies', moviesController.list);
 router.get('/movies/new', moviesController.new);
 router.get('/movies/recommended', moviesController.recommended);
 router.get('/movies/detail/:id', moviesController.detail);
-
+//relacioness y crud
+router.get('/movies/add', moviesController.add);
+router.post('/movies/add', moviesController.create);
+router.get('/movies/edit/:id', moviesController.edit);
+router.put('/movies/edit/:id', moviesController.update);
+router.get('/movies/delete/:id', moviesController.borrar);
+router.delete('/movies/delete/:id', moviesController.destroy);
 
 module.exports = router;
